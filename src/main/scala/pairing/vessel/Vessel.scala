caf8e6d9e333473461461f489d9ec26315240b74
Joseph Lyons <jlyons@bglcorp.com.au>
2017-08-04 12:35:11 +1000

    added validation to constructor

--- src/main/scala/pairing/vessel/Vessel.scala
+++ src/main/scala/pairing/vessel/Vessel.scala
@@ -2,13 +2,21 @@ package pairing.vessel
 
 class Vessel(protected var x: Int, protected var y: Int)
 {
+
+  checkOutOfBounds(x)
+  checkOutOfBounds(y)
+
+  def checkOutOfBounds(x: Int): Unit = {
+    if (0 > x || x > 100) throw new Exception("ship out of bounds")
+  }
+
   def getX: Int = x
   def getY: Int = y
 
-  def move(x: Int, y: Int): Unit =
-  {
-    if (0 > (x + this.x) || (x + this.x) > 100) throw new Exception("ship out of bounds")
-    this.x += x
-    this.y += y
+  def move(x: Int, y: Int): Unit = {
+    checkOutOfBounds(x + this.x)
+    checkOutOfBounds(y + this.y)
+    this.x = x
+    this.y = y
   }
 }
