1bec1d74ee88211bd3cc972b47573b5e8920e276
Joseph Lyons <jlyons@bglcorp.com.au>
2017-08-04 12:16:31 +1000

    moved pair class

---
+++ src/main/scala/pairing/Pair.scala
@@ -0,0 +1,9 @@
+package pairing
+
+import pairing.vessel.offense.OffensiveVessel
+import pairing.vessel.support.SupportVessel
+
+class Pair(
+  val supportVessel: SupportVessel,
+  val offensiveVessel: OffensiveVessel
+)
